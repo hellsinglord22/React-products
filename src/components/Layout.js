894cc7f09ac0d2e92a9379ae332484a5c4f773d3
H <heshamscorpe55@gmail.com>
2016-08-04 15:56:24 +0200

    basic setup

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is working</h1>
+      </div>
+    );
+  }
+}
+
+export default Layout;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from 'react-search'; 
+import Layout from './components/Layout';
+import './index.css';
+
+
+ 
+
+ReactDOM.render(
+ <Layout />,
+  document.getElementById('root')
+);
